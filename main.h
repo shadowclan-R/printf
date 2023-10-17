7d05e7649a0938cfd0350ad7d66b0a73a3a331d5
Unthatile <umakhajane@gmail.com>
2023-10-16 23:16:15 -0700

    printf

--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stadarg.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
@@ -9,5 +12,6 @@ int print_dec(va_list args);
 int print_unsigned(va_list args);
 int print_oct(va_list val);
 int print_hex(va_list val)
+int print_HEX(va_list val)
 
 #endif
