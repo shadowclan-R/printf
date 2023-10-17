361e4a0b3c3c9ad9c5ae62e36f44162a44249910
Unthatile <umakhajane@gmail.com>
2023-10-16 21:37:01 -0700

    printf

--- main.h
+++ main.h
@@ -3,8 +3,10 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_int(va_list args)
-int print_dec(va_list args)
-int print_unsigned(va_list args)
+
+int print_int(va_list args);
+int print_dec(va_list args);
+int print_unsigned(va_list args);
+int print_oct(va_list val);
 
 #endif
